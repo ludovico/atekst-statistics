c90ad2ebd3ec63bc0abde08b50c81a635fe64ee2
Eivind Michael Skretting <eivind@linux.com>
2015-10-07 15:20:22 +0200

    More stuff working

--- analyze.pl
+++ analyze.pl
@@ -2,11 +2,12 @@
 
 use strict;
 use warnings;
+use open qw/:std :utf8/;
 
 
 use lib ('./');
 use Exclude;
-use Dumpvalue;
+use Data::Dumper;
 binmode(STDOUT, ":utf8");
 
 # Setup
@@ -33,16 +34,26 @@ my %totalStats;
 while(my $row = <$fh>) {
 	chomp($row);
 
-	if($newArticle == 1) {
-		$newArticle = 0;
-		next;
-	}
-
 	# Read publication and date
 	if($readTitle == 1) {
-		($publication, $year) = split('/, \d\+\.\d\+\./', $row);
+		my $loff = $row;
+		if($loff =~ /^\s*$/) {
+			$readTitle = 0;
+			next;
+		}
 
-		$readTitle = 0;
+		my $loff2 = $row;
+		if($loff2 !~ /, \d+\.\d+\.\d+/) {
+			next;
+		}
+
+
+		# Extract year, publisher
+		($publication, $year) = split(/, \d+\.\d+\./, $row);
+
+		# "clean" $year var
+		$year = substr $year, 0, 4;
+
 		next;
 	}
 
@@ -52,10 +63,10 @@ while(my $row = <$fh>) {
 	}
 
 	# If finished article, print collected data (DB later)
-	if($row eq $articleSeparator) {
-		$newArticle = 1;
-		next;
-	}
+	#if($row eq $articleSeparator) {
+	#$newArticle = 1;
+	#next;
+	#}
 
 	# Needs publication and year data
 	if($publication eq "" || $year == 0) {
@@ -63,16 +74,10 @@ while(my $row = <$fh>) {
 	}
 
 	foreach my $str ($row =~ /\w+/g) {
-		#$articleStats{lc($str)}++;
 		$totalStats{lc($year)}{lc($publication)}{lc($str)}++;
 	}
 }
 
-my $dump = Dumpvalue->new;
-print $dump.stringify(%totalStats);
-#print "Total statistics\n-----------------\n";
-#foreach my $str (sort { $totalStats{$a} <=> $totalStats{$b} }  keys %totalStats) {
-#if(!shouldExclude($str)) {
-#printf "%-31s %s\n", $str, $totalStats{$str};
-#}
-#}
+print "øØØØlkjåqwerlkj";
+print Dumper(%totalStats);
+
